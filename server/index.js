f525677c774d4b147ccda94e1147b591a0377096
Junsu <pksjmh5295@gmail.com>
2023-02-15 16:30:33 +0900

    day16 - 9th

--- server/index.js
+++ server/index.js
@@ -10,6 +10,21 @@ server.listen(port, () => {
 })
 
 server.on("request", (req, res) => {
-    let data = fs.readFileSync("./package.json", "utf8")
-    res.end(data)
+    // "https://gateway.ipfscdn.io/ipfs/QmYJuWDW6HSC2ArU3xNQMPe8mBXgGWFDYqYAzsSjoZQF29/junsu_sig001.png"
+    if (req.url === "/bear") {
+        let filename = "./public/images/junsu_sig001.png"
+        fs.readFile(filename, (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.writeHead(200, { "Content-type": "image/png" })
+                res.write(data)
+                res.end()
+            }
+        })
+    }
+    if (req.url === "/") {
+        let data = fs.readFileSync("./package.json", "utf8")
+        res.end(data)
+    }
 })
