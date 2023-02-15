c6d51d2317e3251a8c12f53d18ed54ecd8842a3b
Junsu <pksjmh5295@gmail.com>
2023-02-15 15:45:26 +0900

    day16 - 7th

--- server/index.js
+++ server/index.js
@@ -5,7 +5,15 @@ const fs = require("fs")
 var port = 3001
 var server = http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/html; charset=utf8" })
-    response.end("Hello node.js!!")
+    if (request.url === "/profile") {
+        response.write("<h1>프로필 페이지</h1>")
+        response.write("<a href='/'>홈으로 이동</a>")
+    }
+    if (request.url === "/") {
+        response.write("<h1>홈페이지</h1>")
+        response.write("<a href='/profile'>프로필로 이동</a>")
+    }
+    response.end()
 })
 
 server.listen(port, () => {
